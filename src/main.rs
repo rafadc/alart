bc117104ea186d3eb6518677fdfe371bbc77d72d
Rafa de Castro <rafael@micubiculo.com>
2020-08-27 02:03:31 +0200

    Drawing a sample sphere

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,40 @@ mod spheres;
 mod transformations;
 mod tuples;
 
+use crate::colors::Color;
+use crate::intersections::hit;
+use crate::rays::Ray;
+use crate::spheres::Sphere;
+use crate::tuples::*;
+
 fn main() {
     pretty_env_logger::init();
 
     log::info!("Staring alart");
     let mut canvas = canvas::build_canvas(100, 100);
+
+    let mut sphere = Sphere::new();
+    sphere.transformation = transformations::translation(0.0, 0.0, 3.0);
+    for i in 0..100 {
+        for j in 0..100 {
+            let ray = Ray {
+                origin: point(0.0, 0.0, 0.0),
+                direction: vector(-0.5 + (i as f32) * 0.01, -0.5 + (j as f32) * 0.01, 1.0),
+            };
+            if hit(sphere.intersect(&ray)).is_some() {
+                canvas::write_pixel(
+                    &mut canvas,
+                    i,
+                    j,
+                    Color {
+                        r: 1.0,
+                        g: 0.0,
+                        b: 0.0,
+                    },
+                );
+            }
+        }
+    }
 
     canvas::export_png(&canvas, "sample.png");
 }
